792bd688587940acd64627676934de2c0e187daa
Svetlana Sycheva <svetlanasych@gmail.com>
2024-02-05 18:23:21 +0200

    Default SQLAl set

--- settings.py
+++ settings.py
@@ -10,6 +10,6 @@ SHORT_MAX_STRING = 16
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
